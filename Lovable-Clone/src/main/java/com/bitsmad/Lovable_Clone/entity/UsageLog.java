2a66ac086172cfe97d3fbe2ec8a52e0b61b3c7c7
Shivashish Tripathi <Shivashishtipathi227806@gmail.com>
2026-02-14 15:52:59 +0530

    feat: Add `UsageLog` entity for tracking application usage details.

---
+++ Lovable-Clone/src/main/java/com/bitsmad/Lovable_Clone/entity/UsageLog.java
@@ -0,0 +1,15 @@
+package com.bitsmad.Lovable_Clone.entity;
+
+import java.time.Instant;
+
+public class UsageLog {
+    Long id;
+    User user;
+    Project project;
+    String action;
+    Integer tokensUsed;
+    Integer durationMs;
+    String metaData;    //JSON of {model_used,prompt_used}
+    Instant createdAt;
+
+}
